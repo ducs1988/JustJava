2f91515cc2abe16d3a50e4134603dc328fad9901
Chenshi <ducs1988@gmail.com>
2016-06-28 22:11:33 -0500

    Add checkbox code

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -21,8 +22,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCream);
         displayMessage(priceMessage);
+    }
+
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
+        String priceMessage;
+        priceMessage = "Add whipped cream? " + hasWhippedCream;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+
+        return priceMessage;
+    }
+
+    private int calculatePrice() {
+        return quantity * 5;
     }
 
     /**
